06025a02c763c0b510280b4f6f5e6f11a1554fda
shahrour <ahmadshahrour951@gmail.com>
2021-02-11 23:06:18 +0400

    Update models.py

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -19,6 +19,9 @@ class GroceryStore(db.Model):
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
 
+    def __repr__(self):
+      return f'{self.title}, address: {self.address}'
+
 class GroceryItem(db.Model):
     """Grocery Item model."""
     id = db.Column(db.Integer, primary_key=True)
@@ -29,3 +32,6 @@ class GroceryItem(db.Model):
     store_id = db.Column(
         db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
+
+    def __repr__(self):
+      return f'{self.name}, price: {self.price}'

34b92006ad665032cafb9d96f3f8d324c8a5b253
shahrour <ahmadshahrour951@gmail.com>
2021-02-15 05:20:36 +0400

    Adding Forms

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField, FloatField
+from wtforms import StringField, SelectField, SubmitField, FloatField, PasswordField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, URL
-from grocery_app.models import GroceryStore
+from grocery_app.models import GroceryStore, User
 
 
 class GroceryStoreForm(FlaskForm):
@@ -27,3 +27,22 @@ class GroceryItemForm(FlaskForm):
     store = QuerySelectField(
         'Store Name', query_factory=store_query)
     submit = SubmitField('Submit')
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('User Name',
+                           validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name',
+                           validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
